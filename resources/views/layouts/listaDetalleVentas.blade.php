7dfe2d844c5c2f8c2d79d6359f3807a6a7800782
lilyaiza <lilyaiza@gmail.com>
2019-03-20 14:48:39 +0100

    error al reemplazar solucionado

--- resources/views/layouts/listaDetalleVentas.blade.php
+++ resources/views/layouts/listaDetalleVentas.blade.php
@@ -1,22 +1,23 @@
 <!-- resources/views/layouts/app.blade.php -->
 @include('base')
 @include('components.user')
-<div class="container-fluid">
+<div class="col-md-8" id="reemplazarDocumento">
+<div class="container">
     <div class="row">
         <!-- Titulo  -->
-        <h1 class="containerfluidmargen">GestionDocumental/Ventas</h1>
+        <h1>GestionDocumental/Ventas</h1>
     </div>
     <div class="row">
-        <h2 class="containerfluidmargen">Detalle<!-- Pasar por POST el nombre del cliente + el id del pedido--></h2>
+        <h2>Detalle<!-- Pasar por POST el nombre del cliente + el id del pedido--></h2>
     </div>
     
     <div class="row">
 
-        <div class="col-md-8 containerfluidmargen">
+        <div class="col-md-8">
             @include('data.listadoDocumentos')            
             
         </div>
-        <div class="col-md-1 containerfluidmargen"></div>
-        <div class="col-md-3 containerfluidmargen"  id="errores"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-3"  id="errores"></div>
     </div>
 </div>
