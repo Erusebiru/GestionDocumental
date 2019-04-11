570e76afed288a782eb76612e98c214002d9be51
Erusebiru <Erusebiru@gmail.com>
2019-04-11 18:47:24 +0200

    rutas

--- resources/views/data/listadoVentaCliente.blade.php
+++ resources/views/data/listadoVentaCliente.blade.php
@@ -16,7 +16,7 @@
 <div id="prueba2"></div>
 <script>
     var ventas = {!! json_encode($venta->toArray(), JSON_HEX_TAG) !!}['data'];
-    generarTablas("#prueba2",ventas,window.location.pathname+"cliente/detalle/");
+    generarTablas("#prueba2",ventas,+"cliente/detalle/");
 </script>
 
 
