c34a6b6e424b902286f6e7ad7608dcdaa32008e4
Erusebiru <Erusebiru@gmail.com>
2019-04-11 17:48:16 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/gescliadm',['uses' => 'ClientesController@getClientesApi', 'as' => '/']);
+Route::get('/',['uses' => 'ClientesController@getClientesApi', 'as' => '/']);
 
 Route::get('/gescliadm/cliente/{id}', ['uses' => 'ClientesController@getCliente', 'as' => 'cliente']);
 
