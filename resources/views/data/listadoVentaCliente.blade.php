0830d13a952fc29e6d32a61c1adbbf474d661be1
Erusebiru <Erusebiru@gmail.com>
2019-04-11 18:57:29 +0200

    rutas

--- resources/views/data/listadoVentaCliente.blade.php
+++ resources/views/data/listadoVentaCliente.blade.php
@@ -7,7 +7,7 @@
        <script>
        
             var cliente = {!! json_encode($cliente->toArray(), JSON_HEX_TAG) !!}[0] ;
-            var link = $("<a>").attr("href",window.location.pathname+"nuevaVenta/"+cliente["Id"]).text("Nueva Venta").attr("class","btn btn-primary nuevaVentaBtn");
+            var link = $("<a>").attr("href",window.location.pathname.split('cliente')[0]+"nuevaVenta/"+cliente["Id"]).text("Nueva Venta").attr("class","btn btn-primary nuevaVentaBtn");
             $("#divNuevaVenta").append(link);
        </script>
        <div class="col-md-8"><br></div>
@@ -16,7 +16,7 @@
 <div id="prueba2"></div>
 <script>
     var ventas = {!! json_encode($venta->toArray(), JSON_HEX_TAG) !!};
-    generarTablas("#prueba2",ventas.data,window.location.pathname+"cliente/detalle/");
+    generarTablas("#prueba2",ventas.data,window.location.pathname.split('cliente')[0]+"cliente/detalle/");
     console.log(window.location)
 </script>
 
